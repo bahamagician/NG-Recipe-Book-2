b33c04d96f75e21145dbe2c7c2e2f72232b455d3
Chris Thronebury <cthronebury@gmail.com>
2016-10-31 12:22:14 -0400

    Update to new CLI

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe';
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: './recipe-list.component.html'
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
+  recipe = new Recipe('Dummy', 'Dummy', 'http://placehold.it/350x150');
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
